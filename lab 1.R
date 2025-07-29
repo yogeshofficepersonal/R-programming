a48cf4d97c4a3ded8ed00f01159a4c500c42533d
yogesh <yogeshofficial55@gmail.com>
2025-07-29 14:15:16 +0530

    Add files via upload

---
+++ lab 1.R
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+install.packages("ggplot2")
+
+
+
+
+?qplot()
+?ggplot()
+?diamonds
+
+library(ggplot2)
+
+qplot(data=diamonds,carat,price,
+      colour=clarity, facets=.~clarity)
---
+++ lab programs.R
@@ -0,0 +1,25 @@
+?rnorm(n=5,sd=8)
+
+x<- c("a","b","c")
+
+c()
+seq(from=10, to=20, length.out=100)
+rep()
+
+print()
+
+
+is.numeric()
+is.integer()
+is.double()
+is.character()
+
+typeof()
+
+sqrt()
+paste()
+
+
+
+
+#?
